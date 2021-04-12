9055307abbf1f9f7fff715f5422f4f2bbaf0f3a4
MeiyaLian <mlia0004@student.monash.edu>
2021-04-12 14:39:41 +1000

    add documentation

--- test.py
+++ test.py
@@ -5,6 +5,9 @@ import sys
 
 
 def convert_to_json(line_number, data ):
+     """
+        convert a line in the twitter data file into json format 
+     """
     if line_number > 0: #ignore the first line of file 
         data = data.strip()
         if data[-2] == "]":
@@ -17,6 +20,13 @@ def convert_to_json(line_number, data ):
 
 
 def process_tweets(data_file, rank, no_of_process, grids_arr, score_counter ):
+    """
+        This function read the twitter data file and process certain lines based on the rank 
+        of the process. 
+
+        input: data_file(string), rank(int>=0),  no_of_process(int >=1), grids_arr(list), score_counter( ScoreCounter object)
+        output: a dict contains the total number of tweets and the corresponding scores for each cell area
+     """
     statistics = dict()
     for grid in grids_arr:
         statistics[grid["id"]] = [0,0] # each cell in dict stores [number_of_tweets, overall_score ]
@@ -29,7 +39,7 @@ def process_tweets(data_file, rank, no_of_process, grids_arr, score_counter ):
                     if tweet is not None: 
                         tweet = json.loads(tweet)
                         location = locate_coord(grids_arr, tweet["value"]["geometry"]["coordinates"])
-                        if location is not None:
+                        if location is not None: #if the location is in one of the given cell, calculate its score
                             score = score_counter.countScore(tweet["value"]["properties"]["text"])
                             previous = statistics.get(location)
                             statistics[location] = [previous[0]+1, previous[1]+score ]
@@ -42,6 +52,12 @@ def process_tweets(data_file, rank, no_of_process, grids_arr, score_counter ):
 
 
 def parse_grid(fname):
+    """
+        This function parse the melbGrid file and sort the grids based on the left-down priority.
+
+        input: fname(string)
+        output: an array of sorted grids
+    """
     grids = []
     row_boundaries = list()
     col_boundaries = list()
@@ -56,6 +72,12 @@ def parse_grid(fname):
     return grids
 
 def locate_coord(grids, coord):
+    """
+        This function takes a coordinate and determine which cell does it belong to
+
+        input: grids ( grids array), coord ( coordinates, [x_coord, y_coord])
+        output: the cell it belongs to(None if it doesnt belong to any)
+    """
     for i in range(len(grids)):
         if coord[0]>= grids[i]["xmin"] and coord[0] <=grids[i]["xmax"] and coord[1]>=grids[i]["ymin"] and coord[1] <= grids[i]["ymax"]:
             return grids[i]["id"]
@@ -101,10 +123,10 @@ class ScoreCounter:
 
     def process_dict(self, file_name):
         """
-        This function creates a dict trie based on the given dictionary file.
+            This function creates a dict trie based on the given dictionary file.
 
-        input: file_name (string), the file path of the dictionary file
-        output: None 
+            input: file_name (string), the file path of the dictionary file
+            output: None 
         """
         with open(file_name) as f:
             for line in f:
@@ -119,16 +141,16 @@ class ScoreCounter:
     
     def countScore(self,sentence):
         """
-        This function is used for calculating the sentiment socre of the given string.
-        The function examines the input string char by char and traverse the dict trie from the root, tries to find  
-        a furthest node containing a score that matches the current char in the string.
-        If there is no children node corresponding to the current char, it means there is no words to match 
-        
-        the function will restart to traverse from the root. 
+            This function is used for calculating the sentiment socre of the given string.
+            The function examines the input string char by char and traverse the dict trie from the root, tries to find  
+            a furthest node containing a score that matches the current char in the string.
+            If there is no children node corresponding to the current char, it means there is no words to match 
+            
+            the function will restart to traverse from the root. 
 
 
-        input: sentence (string)
-        output: the score of the string (integer >= 0)
+            input: sentence (string)
+            output: the score of the string (integer >= 0)
         """
         sentence = sentence.lower()
         score = 0 
@@ -136,47 +158,38 @@ class ScoreCounter:
         matched_score = 0 
         current_index = 0 
         word_len = 0
-        next_starting_index = -1 
         exception = "\"\'‘’“”?! ,.，。"
         current_node = self.trie.root
         while current_index < len(sentence):
-            current_char = sentence[current_index]
-            current_node = current_node.children.get(current_char) 
-
-            if current_node is None :
+            current_char = current_node.children.get(sentence[current_index]) 
+            if current_char is None :
                 if matched_index != -1 and (matched_index-word_len <0 or sentence[matched_index-word_len] in exception) and ( sentence[matched_index+1] in exception ):
-        
                     score += matched_score
                     current_index = matched_index + 1 
                     matched_index = -1
 
                 else:
-                    if next_starting_index != -1:
-                        current_index = next_starting_index
-                        next_starting_index = -1
-                    # if sentence[current_index-1] in exception and current_node!= self.trie.root:
-                    #     pass
+                    if sentence[current_index-1] in exception and current_node!= self.trie.root:
+                        pass
                     else:
                         current_index +=1
                         while current_index < len(sentence) and (not (sentence[current_index-1] in exception)):
                             current_index +=1
                 current_node = self.trie.root
             else:
-                if current_node.is_word:
+                if current_char.is_word:
                     matched_index = current_index
-                    matched_score = current_node.score
-                    word_len = current_node.index
+                    matched_score = current_char.score
+                    word_len = current_char.index
                 
                     if current_index == len(sentence)-1 and (matched_index-word_len <0 or sentence[matched_index-word_len] in exception):
                         score += matched_score
                 
-                if current_char.isspace() and next_starting_index == -1:
-                    next_starting_index = current_index + 1 
-
                 current_index +=1
-              
+                current_node = current_char
         return score
 
+    
 
 def main(argv):
    
